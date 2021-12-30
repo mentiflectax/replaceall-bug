25e8a40c1fa4f2f55cfb877321c7b08325a3a7a9
Dmitrii Pisarenko <dp@dpisarenko.com>
2021-12-30 10:17:31 +0100

    Added another test.

--- src/test/java/ReplaceAllTtest.java
+++ src/test/java/ReplaceAllTtest.java
@@ -4,15 +4,29 @@ import static org.junit.Assert.assertEquals;
 
 public class ReplaceAllTtest {
     @Test
-    public void replaceAllTest() {
+    public void test1() {
         // Given
         final String text = "#+begin_quote\nSome text\n#+end_quote";
         final String searchItem = "#+begin_quote";
 
         // When
-        final String actualResult = text.replaceAll(text, searchItem);
+        final String actualResult = text.replaceAll(searchItem, "");
 
         // Then
-        assertEquals("\\nSome text\\n#+end_quote", actualResult);
+        assertEquals("\nSome text\n#+end_quote", actualResult);
     }
+
+    @Test
+    public void test2() {
+        // Given
+        final String text = "#+begin_quote\nSome text\n#+end_quote";
+        final String searchItem = "#";
+
+        // When
+        final String actualResult = text.replaceAll(searchItem, "");
+
+        // Then
+        assertEquals("+begin_quote\nSome text\n+end_quote", actualResult);
+    }
+
 }
