b8d350e12aa3aa9d0d9024ccf3491bf07fabef84
Dmitrii Pisarenko <dp@dpisarenko.com>
2021-12-30 10:23:00 +0100

    Found out the solution.

--- src/test/java/ReplaceAllTtest.java
+++ src/test/java/ReplaceAllTtest.java
@@ -7,7 +7,7 @@ public class ReplaceAllTtest {
     public void test1() {
         // Given
         final String text = "#+begin_quote\nSome text\n#+end_quote";
-        final String searchItem = "#+begin_quote";
+        final String searchItem = "#\\+begin_quote";
 
         // When
         final String actualResult = text.replaceAll(searchItem, "");
